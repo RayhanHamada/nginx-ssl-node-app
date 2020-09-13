dceb03843ea337c22b7384a8df48ad55228b13cd
RayhanHamada <hamadadev2016@gmail.com>
2020-09-13 16:19:03 +0700

    add item to .gitignore and fix typo

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const port = 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 app.get("/", (_req, res) => {
-  res.send("<h1>Hello, this is my node app</h1>").end();
+  res.send("<h1>Hello, this is my Node App</h1>").end();
 });
